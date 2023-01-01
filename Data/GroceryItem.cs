2c22dfbbf4351e3a98454a21d3bd6564c12e6ff0
eking <elijahgideonking@gmail.com>
2023-01-01 11:30:03 -0500

    grocery form with validation

--- Data/GroceryItem.cs
+++ Data/GroceryItem.cs
@@ -1,8 +1,16 @@
-﻿namespace ShoppingList.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList.Data
 {
     public class GroceryItem
     {
+        [Required]
+        [StringLength(20, ErrorMessage = "Name must contain less than 20 characters.")]
+        [MinLength(2, ErrorMessage = "Name must contain at least 2 characters.")]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Valid price range is (1-1000)")]
         public decimal Price { get; set; }
     }
 }
